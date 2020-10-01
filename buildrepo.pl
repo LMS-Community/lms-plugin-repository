b3e0dd769341f78875ad13dedf1c1f8c8fc2a31c
Michael Herger <michael@herger.net>
2020-10-01 18:51:47 +0200

    Commit initial extensions.xml file and build script for plugin repository.

---
+++ buildrepo.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/perl
+
+use strict;
+
+use JSON;
+use LWP::UserAgent;
+use XML::Simple;
+use Data::Dumper;
+
+use constant INCLUDE_FILE => 'include.json';
+use constant REPO_FILE    => 'extensions.xml';
+
+my $includes;
+
+eval {
+	open my $fh, '<', INCLUDE_FILE;
+	$/ = undef;
+	$includes = decode_json(<$fh>);
+	close $fh;
+} || die "$@";
+
+my $ua = LWP::UserAgent->new(
+	timeout => 5,
+	ssl_opts => {
+		verify_hostname => 0
+	}
+);
+
+$ua->agent('Mozilla/5.0, LMS buildrepo');
+
+my $out = {
+	details => {
+		title => $includes->{title}
+	}
+};
+
+for my $url (sort @{$includes->{repositories}}) {
+
+	my $resp = $ua->get($url);
+	my $content;
+
+	if (!$resp->is_success) {
+
+		warn "error fetching $url - " . $resp->status_line . "\n";
+
+		if ($resp->code == 500) {
+			warn "trying curl instead...\n";
+			$content = `curl -m35 -L -s $url`;
+			$content =~ s/^\s+|\s+$//g;
+		}
+	} else {
+		$content = $resp->content;
+	}
+
+	if ($content) {
+		print "$url\n";
+
+		my $xml = eval { XMLin($content,
+			SuppressEmpty => 1,
+			KeyAttr    => [],
+			ForceArray => [ 'applet', 'wallpaper', 'sound', 'plugin', 'patch' ],
+		) };
+
+		if ($@) {
+			warn "bad xml ($url) $@";
+			next;
+		}
+
+		for my $content (qw(applet wallpaper sound plugin patch)) {
+			my $element = $content."s";
+			$element =~ s/patchs/patches/;
+			for my $item (@{ $xml->{"${element}"}->{"$content"} || [] }) {
+				my $name = $item->{'name'};
+
+				print "  $content $name\n";
+				push @{ $out->{"${element}"}->{"$content"} ||= [] }, $item;
+			}
+		}
+	}
+}
+
+XMLout($out,
+	OutputFile => REPO_FILE,
+	RootName   => 'extensions',
+	KeyAttr    => [ 'name' ],
+);
+
+1;
