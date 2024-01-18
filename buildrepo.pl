14854aa9633da4254df418824f4178284fe2f55c
Michael Herger <michael@herger.net>
2024-01-18 23:12:06 +0100

    Add categories to the plugin repository

--- buildrepo.pl
+++ buildrepo.pl
@@ -5,10 +5,110 @@ use strict;
 use JSON;
 use LWP::UserAgent;
 use XML::Simple;
-#use Data::Dumper;
+# use Data::Dumper;
 
 use constant INCLUDE_FILE => 'include.json';
 use constant REPO_FILE    => 'extensions.xml';
+
+my $categoriesMap = {
+	'1001Albums' => 'musicservices',
+	'Accuradio' => 'radio',
+	'AlternativePlayCount' => 'playlists',
+	'ARDAudiothek' => 'radio',
+	'AutoDisplay' => '',
+	'AutoRepo' => '',
+	'Bandcamp' => 'musicservices',
+	'BBCSounds' => 'radio',
+	'BookmarkHistory' => '',
+	'C3PO' => '',
+	'CastBridge' => 'hardware',
+	'CBCCanadaFrancais' => 'radio',
+	'ClientCleanup' => '',
+	'CommunityFirmware' => '',
+	'CustomBrowse' => '',
+	'CustomClockHelper' => '',
+	'CustomScan' => '',
+	'CustomSkip' => '',
+	'CustomSkip3' => 'playlists',
+	'CustomStartStopTimes' => '',
+	'CustomTagImporter' => 'scanning',
+	'DarkDefaultSkin' => 'skin',
+	'DatabaseQuery' => '',
+	'DenonAvpControl' => 'hardware',
+	'DisableShuffle' => '',
+	'DRS' => 'radio',
+	'DynamicMix' => '',
+	'DynamicPlayList' => '',
+	'DynamicPlaylistCreator' => 'playlists',
+	'DynamicPlaylists4' => 'playlists',
+	'FilesViewer' => '',
+	'FileViewer' => '',
+	'GlobalPlayerUK' => 'radio',
+	'Groups' => '',
+	'HideMenus' => '',
+	'IgnoreDirREManager' => '',
+	'iHeartRadio' => 'radio',
+	'ImageProxy' => '',
+	'InformationScreen' => '',
+	'InguzEQ' => 'audioformats',
+	'iPeng' => 'skin',
+	'IRBlaster' => 'hardware',
+	'LastMix' => 'musicservices',
+	'LicenseManagerPlugin' => '',
+	'Live365' => 'radio',
+	'LocalPlayer' => '',
+	'MaterialSkin' => 'skin',
+	'MHCPAN' => '',
+	'MixCloud' => 'musicservices',
+	'MultiLibrary' => '',
+	'MusicArtistInfo' => 'musicservices',
+	'MusicInfoSCR' => '',
+	'NoSetup' => '',
+	'PhilsLibraries' => '',
+	'Phishin' => 'radio',
+	'PlanetRadio' => 'radio',
+	'PlayHistory' => '',
+	'PlaylistGenerator' => '',
+	'PlaylistMan' => '',
+	'PodcastExt' => '',
+	'RadioFavourites' => 'radio',
+	'RadioFeedsSBS' => 'radio',
+	'RadioFrance' => 'radio',
+	'RadioNowPlaying' => 'radio',
+	'RadioParadise' => 'radio',
+	'RaopBridge' => 'hardware',
+	'RatingButtons' => '',
+	'RatingsLight' => '',
+	'Reliable' => '',
+	'ShairTunes2W' => 'hardware',
+	'SigGen' => '',
+	'SimpleLibraryViews' => '',
+	'SongFileViewer' => '',
+	'SongInfo' => '',
+	'SongLyrics' => '',
+	'Spotty' => 'musicservices',
+	'SpottyARMLegacyBin' => 'musicservices',
+	'SpottyARMPi0Bin' => 'musicservices',
+	'SpottyBinFreeBSD' => 'musicservices',
+	'Spottyi86pcsolarisBin' => 'musicservices',
+	'SQLPlayList' => '',
+	'SqueezeCLIHandler' => '',
+	'SugarCube' => 'musicservices',
+	'SwitchGroupPlayer' => 'playlists',
+	'TimeSpeller' => '',
+	'TimesRadio' => 'radio',
+	'TitleSwitcher' => '',
+	'TrackStatPlaylist' => '',
+	'UPnPBridge' => 'hardware',
+	'VirginRadio' => 'radio',
+	'VirtualLibraryCreator' => '',
+	'VisualStatistics' => 'statistics',
+	'WalkWithMe' => '',
+	'Wefunk' => 'radio',
+	'xAP' => '',
+};
+
+my %categories = map { $_ => 1 } values %$categoriesMap;
 
 my $includes;
 
@@ -71,6 +171,11 @@ for my $url (sort @{$includes->{repositories}}) {
 			$element =~ s/patchs/patches/;
 			for my $item (@{ $xml->{"${element}"}->{"$content"} || [] }) {
 				my $name = $item->{'name'};
+
+				if ($content eq 'plugin') {
+					delete $item->{category} if $item->{category} && !$categories{$item->{category}};
+					$item->{category} ||= $categoriesMap->{$item->{name}} || 'misc';
+				}
 
 				print "  $content $name\n";
 				push @{ $out->{"${element}"}->{"$content"} ||= [] }, $item;
